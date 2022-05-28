ff3e79b15db2f8640de1d2e85a3eb8fef62d1b82
SayantaniDeb <74983536+SayantaniDeb@users.noreply.github.com>
2022-05-29 01:50:05 +0530

    added error msgs

--- apps/upload.py
+++ apps/upload.py
@@ -33,10 +33,12 @@ def app():
             model = Sequential([model, GlobalMaxPooling2D()])
             def save_file(uploaded_file):
                 try:
-                    with open(os.path.join("Snap-Shop/uploader", uploaded_file.name), 'wb') as f:
+                    with open(os.path.join("uploader", uploaded_file.name), 'wb') as f:
+                        st.header(f)
                         f.write(uploaded_file.getbuffer())
                         return 1
                 except:
+                    st.header("File not saved")
                     return 0
 
 
